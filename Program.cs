fa661a04e4889d294a0d4af205d3815dee2be749
GilGuereque <gguereque326@gmail.com>
2024-01-15 19:20:29 -0800

    finalized and refined the simple console app battle game.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
-﻿using TurnBasedGame;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnBasedGame;
 
-Unit player = new Unit(100, 25, 13, "Player1");
-Unit enemy = new Unit(80, 11, 8, "Lich King");
-
-Console.WriteLine("It is the player turn! What will you choose to do?");
-string playerChoice = Console.ReadLine();
-
-if (playerChoice == "attack")
+namespace TurnBasedGame
 {
-    player.Attack(enemy);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit player = new Unit(100, 25, 13, "Player1");
+            Unit enemy = new Unit(80, 11, 8, "Lich King");
+            Random random = new Random();
+            // while loop to keep game going if the player or enemy is not dead
+            while(!player.IsDead && !enemy.IsDead)
+            { 
+                Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+                Console.WriteLine("It is the player turn now! What will you choose to do? Enter in your choice: 'attack' or 'heal' ");
+                string playerChoice = Console.ReadLine();
+
+                if (playerChoice == "attack")
+                {
+                    player.Attack(enemy);
+                }
+                else
+                {
+                    player.Heal();
+                }
+                // if player or enemy is dead break out of the loop
+                if (player.IsDead || enemy.IsDead) break;
+                Console.WriteLine(player.UnitName + " HP = " + player.Hp + ". " + enemy.UnitName + " HP = " + enemy.Hp);
+
+                Console.WriteLine("It is now the enemy turn!");
+                // logic using Random to decide if 0 the enemy attacks, else it will heal
+                int rand = random.Next(0, 2);
+
+                if (rand == 0)
+                {
+                    enemy.Attack(player);
+                }
+                else
+                {
+                    enemy.Heal();
+                }
+
+            }
+        }
+    }
 }
+
+
+
+
--- Unit.cs
+++ Unit.cs
@@ -15,6 +15,13 @@ namespace TurnBasedGame
         private string unitName;
         private Random random;
 
+        public int Hp { get { return currentHp; } }
+
+        public string UnitName { get { return unitName; } }
+
+        public bool IsDead { get { return currentHp <= 0; } }
+
+        // Creating a Unit constructor to use for our enemy & player classes
         public Unit(int maxHp, int attackPower, int healPower, string unitName)
         {
             this.maxHp = maxHp;
@@ -25,18 +32,36 @@ namespace TurnBasedGame
             this.random = new Random();
         }
 
+        // Build out the Attack method 
         public void Attack(Unit unitToAttack)
         {
             double rng = random.NextDouble();
-            rng = rng / 2 + 0.75f;
+            rng = rng / 2 + 0.75f; // Change the dmg range betweent 0.75% to 1.25%
             int randDamage = (int)(attackPower * rng);
             unitToAttack.TakeDamage(randDamage);
             Console.WriteLine(unitName + " attacks " + unitToAttack.unitName + " and deals " + randDamage + " damage! Ouch.");
         }
 
+        // TakeDamage method with lowers the Units HP & checks if they are dead yet
         public void TakeDamage(int damage)
         {
             currentHp -= damage;
+
+            if(IsDead)
+            {
+                Console.WriteLine(UnitName + " has been defeated! Victory!");
+            }
+        }
+
+        // Heal method the player or enemy can use to heal themselves
+        public void Heal()
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int heal = (int)(rng * healPower);
+            currentHp = heal + currentHp > maxHp ? maxHp : currentHp + heal;
+            Console.WriteLine(UnitName + " heals by " + heal + " points. Nice!");
         }
     }
+
 }
